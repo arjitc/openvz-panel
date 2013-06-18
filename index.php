a90b081922b430ad0d00748087317d6c32495d21
Arjit Chaudhary <arjit@ArjitNetbook.(none)>
2013-06-18 17:48:41 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * A simple, clean and secure PHP Login Script
+ * 
+ * ADVANCED VERSION
+ * (check the website / github / facebook for other versions)
+ * 
+ * A simple PHP Login Script without all the nerd bullshit.
+ * Uses PHP SESSIONS, modern SHA512-password-hashing and salting
+ * and gives the basic functions a proper login system needs.
+ * 
+ * @package php-login
+ * @author Panique <panique@web.de>
+ * @link https://github.com/panique/php-login/
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+// if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
+// (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+require_once("libraries/password_compatibility_library.php");
+
+// include the configs / constants for the database connection
+require_once("config/db.php");
+
+// include the hashing cost factor (you can delete this line if you have never touched the cost factor,
+// the script will then use the standard value)
+require_once("config/hashing.php");
+
+// load the login class
+require_once("classes/Login.php");
+
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process.
+$login = new Login();
+
+// ... ask if we are logged in here:
+if ($login->isUserLoggedIn() == true) {
+    // the user is logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are logged in" view.
+    include("views/logged_in.php");
+    
+} else {
+    // the user is not logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are not logged in" view.
+    include("views/not_logged_in.php");
+}
---
+++ register.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * A simple, clean and secure PHP Login Script
+ * 
+ * ADVANCED VERSION
+ * (check the website / github / facebook for other versions)
+ * 
+ * A simple PHP Login Script without all the nerd bullshit.
+ * Uses PHP SESSIONS, modern password-hashing and salting
+ * and gives the basic functions a proper login system needs.
+ * 
+ * @package php-login
+ * @author Panique <panique@web.de>
+ * @link https://github.com/panique/php-login/
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+// if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
+// (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+require_once("libraries/password_compatibility_library.php");
+
+// include the configs / constants for the database connection
+require_once("config/db.php");
+
+// include the hashing cost factor (you can delete this line if you have never touched the cost factor,
+// the script will then use the standard value)
+require_once("config/hashing.php");
+
+// include the configs / constants for the verification URL
+require_once("config/email_verification.php");
+
+//load the registration class
+require_once("classes/Registration.php");
+
+// create the registration object. when this object is created, it will do all registration stuff automatically
+// so this single line handles the entire registration process.
+$registration = new Registration();
+
+// showing the register view (with the registration form, and messages/errors)
+include("views/register.php");
